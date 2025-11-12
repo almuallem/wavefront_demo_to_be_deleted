37b85a856665744c951eb9cd9d1c4f9e2f71dc46
Ali Almuallem <19761263+almuallem@users.noreply.github.com>
2025-11-12 18:04:44 -0500

    Update frame counter calculation in demo_run_experiment

--- utils/demo_run_experiment.py
+++ utils/demo_run_experiment.py
@@ -341,7 +341,7 @@ def create_gif_with_colorbar(GS_preds_np, discard_every = 5, output_dir="output"
         ax.set_title(plot_title, color='black', fontsize=12)#, backgroundcolor='black')
         
         # Add the frame counter text
-        current_iter = 5 * (i)
+        current_iter = discard_every * (i)
         counter_text = f"Iter: {current_iter} / {total_iterations}"
         ax.text(
             0.05, 0.95, 
